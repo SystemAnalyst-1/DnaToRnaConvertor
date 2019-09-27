98de8b10c715431ae29a2a43df477e3632ad3077
teeara1991 <teeara1991@yandex.ru>
2019-09-27 23:46:44 +0300

    changed method replacing letters in dnaToRna() and rnaToDna(), renamed haveOnlyCertainLetters function to isValid.

--- src/convert.js
+++ src/convert.js
@@ -1,50 +1,25 @@
 /* 
-haveOnlyCertainLetters(str, reverse = false) is validating of a string, if string
-is not correct, it`ll return -1.
+isValid(str, reverse = false) is validating of a string.
 dnaToRna(str) function is converting DNA to RNA. It'll return
 converted string or -1 if it was error.
 rnaToDna(str) function is converting RNA to DNA. It'll return
 converted string or -1 if it was error.
 */
 
-const DNA = ["G", "C", "T", "A"];
-const RNA = ["C", "G", "A", "U"];
+const DNA = { G: "C", C: "G", T: "A", A: "U", g: "C", c: "G", t: "A", a: "U" };
+const RNA = { G: "C", C: "G", U: "A", A: "T", g: "C", c: "G", u: "A", a: "T" };
+const regexDNA = /^[GCTAgcta]+$/;
+const regexRNA = /^[GCUAgcua]+$/;
 
 //validation of a string
-export const haveOnlyCertainLetters = (str, reverse = false) => {
-  const regexDNA = /^[GCTAgcta]+$/;
-  const regexRNA = /^[GCUAgcua]+$/;
-  let isValid = reverse ? regexRNA.test(str) : regexDNA.test(str);
-  if (!isValid) {
-    return -1;
-  } else {
-    return 1;
-  }
+export const isValid = (str, reverse = false) => {
+  return reverse ? regexRNA.test(str) : regexDNA.test(str);
 };
 //convert dnaToRna
 export const dnaToRna = str => {
-  if (haveOnlyCertainLetters(str) === 1) {
-    let newStr = str.toUpperCase().split("");
-    for (let i = 0; i < newStr.length; i++) {
-      let position = DNA.indexOf(newStr[i]);
-      if (position > -1) newStr[i] = RNA[position];
-    }
-    return newStr.join("");
-  } else {
-    return -1;
-  }
+  return isValid(str) ? str.replace(/[GCTAgcta]/g, m => DNA[m]) : -1;
 };
 //convert rnaToDna
 export const rnaToDna = str => {
-  if (haveOnlyCertainLetters(str, true) === 1) {
-    let newStr = str.toUpperCase().split("");
-    for (let i = 0; i < newStr.length; i++) {
-      let position = RNA.indexOf(newStr[i]);
-      if (position > -1) newStr[i] = DNA[position];
-    }
-
-    return newStr.join("");
-  } else {
-    return -1;
-  }
+  return isValid(str, true) ? str.replace(/[GCUAgcua]/g, m => RNA[m]) : -1;
 };
